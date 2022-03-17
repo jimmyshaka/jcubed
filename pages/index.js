482f446c8b67d175deeb53512c147c734083aac7
James Walsh <jamesandersonwalsh@gmail.com>
2022-03-17 13:08:13 -0600

    Add card for future mom symptom page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Stack } from '@chakra-ui/react'
+import { Stack, Text } from '@chakra-ui/react'
 
 import { Card, Layout } from '../src/components'
 
 export default function Home() {
   return (
     <Layout title="Walsh ♀️♂️ Party">
+     <Text fontSize="md" textAlign="center"> There are old <b>folklore tales</b> that help predict traits about your baby. Please select from the cards below if you would like to hear about Mom&apos;s symptoms, guess the gender, or see how friends and family voted</Text>
       <Stack marginTop="2em" spacing={8}>
+      <Card href="/mom">Mom&apos;s Symptoms</Card>
         <Card href="/vote">Guess Gender⁉️</Card>
         <Card href="/results">See Results 🥳</Card>
       </Stack>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,9 @@
-import { Box, Link } from '@chakra-ui/react'
+import { Box, Link, useBreakpointValue } from '@chakra-ui/react'
 
 export default function Card({ children, ...rest }) {
+  const heightVariant = useBreakpointValue({ base: '8ch', lg: '14ch' })
+  console.log('heighVariant:', heightVariant)
+
   return (
     <Box
       display="flex"
@@ -12,7 +15,7 @@ export default function Card({ children, ...rest }) {
       borderWidth={2}
       boxShadow="2xl"
       color="cyan.50"
-      height="7em"
+      height={heightVariant}
       width="12em"
       padding="0.25em"
       fontSize="2xl"
