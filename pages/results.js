588ccebbadf5e18c5324cee27d83e863875efb28
James Walsh <jamesandersonwalsh@gmail.com>
2022-03-16 22:20:17 -0600

    Improve styling of results page

--- pages/results.js
+++ pages/results.js
@@ -1,4 +1,4 @@
-import { Box, List, ListItem, Heading, Text } from '@chakra-ui/react'
+import { Box, Grid, GridItem, Heading, Text, useBreakpointValue } from '@chakra-ui/react'
 import useSWR from 'swr'
 import { useEffect, useState } from 'react'
 
@@ -8,6 +8,9 @@ import { fetcher } from '../src/utils/api'
 export default function Home() {
   const { data: voteData } = useSWR(`api/votes`, fetcher)
   const [votes, setVotes] = useState([])
+  const gridColumVariant = useBreakpointValue({ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' });
+
+  console.log('gridColumVariant:', gridColumVariant)
 
   useEffect(() => {
     if (voteData) {
@@ -23,41 +26,48 @@ export default function Home() {
       backgroundColor: type == 'girl' ? 'purple.200' : 'cyan.600',
       borderRadius: '8px',
       padding: '0.25em',
-      margin: '0.5em 0',
-      color: 'purple.600',
+      margin: '0.5em',
+      color: type === 'girl' ? 'purple.800' : 'cyan.800',
       minHeight: '10ch',
+      minWidth: '20ch'
     }
     const messageStyles = {
       backgroundColor: type === 'girl' ? 'purple.400' : 'cyan.300',
       borderRadius: '4px',
+      maxWidth: 'fit-content',
+      minWidth: '100%',
+      minHeight: '48px',
+      padding: '4px'
     }
 
     return (
-      <ListItem key={vote.id} {...listItemStyles}>
+      <GridItem key={vote.id} {...listItemStyles}>
         <Text fontSize="xl" color={type === 'girl' ? 'purple.600' : 'cyan.100'}>
           {type === 'boy' ? '♂️' : '♀️'}&nbsp;{vote.name}
         </Text>
         <Box {...messageStyles}>{vote.messageToParents}</Box>
-      </ListItem>
+      </GridItem>
     )
   }
 
   return (
-    <Layout title="Results" id="results-list">
+    <Layout title="👶 🍼 Votes" id="results-list">
       <Box display="flex" flexDirection="row">
         <Box as="section" margin="0.5em">
           <Heading as="h2" color="cyan.400">
             Boy Votes
           </Heading>
-          <List>
+          <Text fontSize="lg">Count: {boyVotes.length}</Text>
+          <Grid templateColumns={gridColumVariant}>
             {boyVotes.map((vote) => voteRenderer(vote, 'boy'))}
-          </List>
+          </Grid>
         </Box>
         <Box as="section" margin="0.5em">
           <Heading as="h2" color="purple.500">Girl Votes</Heading>
-          <List>
+          <Text fontSize="lg">Count: {boyVotes.length}</Text>
+          <Grid templateColumns={gridColumVariant}>
             {girlVotes.map((vote) => voteRenderer(vote, 'girl'))}
-          </List>
+          </Grid>
         </Box>
       </Box>
     </Layout>
