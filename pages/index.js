903b5c5e8c645a40be45af166e8d748f3d8f76e0
James Walsh <jamesandersonwalsh@gmail.com>
2022-03-17 16:52:38 -0600

    Improve verbiage on home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Stack, Text } from '@chakra-ui/react'
 
 import { Card, Layout } from '../src/components'
@@ -8,11 +6,9 @@ export default function Home() {
   return (
     <Layout title="Walsh ♀️♂️ Party">
       <Text fontSize="md" textAlign="center">
-        {' '}
         There are old <b>folklore tales</b> that help predict traits about your
-        baby. Please select from the cards below if you would like to hear about
-        Mom&apos;s symptoms, guess the gender, or see how friends and family
-        voted
+        baby. Check Mom&apos;s symptoms using the cards below then guess the sex of the Walsh baby. Or check in and see how friends & family
+        voted below.
       </Text>
       <Stack marginTop="2em" spacing={8}>
         <Card href="/mom">Mom&apos;s Symptoms</Card>
